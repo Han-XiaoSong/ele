ea421a646fda0d951a08568052ca4595529d95c1
Han XiaoSong <1476103098@qq.com>
2019-10-24 19:05:02 +0800

    first push

---
+++ ele.py
@@ -0,0 +1,73 @@
+
+# -*- coding: UTF-8 -*-
+import requests
+import json
+import pandas as pd
+import os
+from openpyxl import Workbook, load_workbook
+
+
+nameHash = {
+    "Terran": "田鹏",
+    "怜子自甘心ღ": "王帅",
+    "韩小松": "韩小松",
+    "walter": "郑双杰",
+    "chaos": "颜思行",
+    "laughing": "刘华峰",
+    "嘿然": "周泽林",
+    "局长": "程军",
+    "发起人1号订餐人": "韩小松"
+}
+time = "23/10/2019"
+hongbao = 0
+
+url = 'https://h5.ele.me/restapi/booking/v1/carts/cart324d5ba94d6c41d588e456ac46ee574d?sig=ee522f6ac50c05b03af130bf0e8081c7&from=pindan&extras[]=restaurant_info&extras[]=share_pindan&extras[]=order_status&random=0.20294561891709595'
+r = requests.get(url).json()
+
+total = r["total"]
+discount_amount = -r["discount_amount"] + hongbao
+packing_fee = r["extra"]["packing_fee"]["price"]
+agent_fee = r["extra"]["agent_fee"]["price"]
+names = r["pindan"]["owners"]
+order = r["group"]
+
+if not os.path.exists("ele.xlsx"):
+    wb = Workbook()
+    wb.save("ele.xlsx")
+
+subsidy2orderer = packing_fee + agent_fee
+wb = load_workbook("ele.xlsx")
+ws = wb.get_sheet_by_name(u"进出表")
+
+for i in range(len(names)):
+    prename = names[i]["name"]
+    name = nameHash[prename.encode("utf-8")]
+    groupIndex = names[i]["group_index"]
+    orders = order[groupIndex]
+    
+    originalFee = 0 
+    for eachOrder in orders:
+        originalFee += eachOrder["total_price"]
+    
+    discount = round((originalFee * discount_amount / (total - packing_fee - agent_fee)),2)
+    subsidy = 15
+    if ((originalFee - discount) / 2) < 15:
+        subsidy = round((originalFee - discount) / 2, 2)
+    actuallyPay = round((originalFee - discount - subsidy), 2)
+
+    row = len(ws["A"]) + 1    
+    ws.cell(row=row, column=2, value=time) 
+    ws.cell(row=row, column=3, value=actuallyPay)
+    ws.cell(row=row, column=4, value=name)
+    ws.cell(row=row, column=5, value=nameHash["发起人1号订餐人"])
+
+    subsidy2orderer += subsidy
+# wb.save("ele.xlsx")
+
+# wb1 = load_workbook("ele.xlsx")
+ws1 = wb.get_sheet_by_name(u"补助表")
+row1 = len(ws1["A"]) + 1
+ws1.cell(row=row1, column=2, value=time)
+ws1.cell(row=row1, column=3, value=subsidy2orderer)
+ws1.cell(row=row1, column=4, value=nameHash["发起人1号订餐人"])
+wb.save("ele.xlsx")
